4487f10108bfe61c94e66c07ef169362a9f13f95
sam <meisamtaher71@gmail.com>
2024-11-16 13:43:30 +0700

    First version of Contracts

---
+++ script/DeploySwapper.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import {Script} from "forge-std/Script.sol";
+import {Swapper} from "../src/Swapper.sol";
+
+import "forge-std/console.sol";
+
+contract DeploySwapper is Script {
+    address public owner = address(0x123);
+    address public swapRouter = address(0x456); // Replace with actual swapRouter address
+    address public poolAddressesProvider = address(0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A); // sepolia address provider 
+    function run() external {
+        vm.startBroadcast();
+
+        Swapper swapper = new Swapper(owner,swapRouter,poolAddressesProvider);
+
+        console.log("Swapper deployed at:", address(swapper));
+
+        vm.stopBroadcast();
+    }
+}
