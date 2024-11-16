3677e610a6728d2709e72aa00da29aa6c8c3ea92
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2024-11-16 14:07:35 +0700

    add TP/SL

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCallback, useState } from "react";
-import { Token, validTokens } from "@/constants/tokens";
+import { Token } from "@/constants/tokens";
 import { Slider } from "@/components/ui/slider";
 import { TokenSelector } from "../components/ui/token-selector/TokenSelector";
 
@@ -9,6 +9,9 @@ export default function Home() {
   const [pairToken, setPairToken] = useState<Token | null>(null);
   const [volum, setVolum] = useState<string>("");
   const [amount, setAmount] = useState<number>(2);
+  const [tlb, setTlb] = useState<boolean>(false);
+  const [takeProfit, setTakeProfit] = useState<string>("");
+  const [stopLoss, setStopLoss] = useState<string>("");
   const onChangeVolum = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     if (!Number.isNaN(Number(value))) {
@@ -16,13 +19,33 @@ export default function Home() {
     }
   };
 
-  const handleChangeToken = useCallback((token: Token)=> {
-    setPairToken(token)
-  },[setPairToken])
+  const onChangeTakeProfit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (!Number.isNaN(Number(value))) {
+      setTakeProfit(value);
+    }
+  };
+  const onChangeStopLoss = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (!Number.isNaN(Number(value))) {
+      setStopLoss(value);
+    }
+  };
+
+  const handleChangeToken = useCallback(
+    (token: Token) => {
+      setPairToken(token);
+    },
+    [setPairToken]
+  );
+
+  const onChangeTlb = () => {
+    setTlb((prev) => !prev);
+  };
 
   return (
-    <div className="mt-4 flex items-center justify-center p-8 w-full">
-      <div className=" md:w-1/2 w-full text-[#462684]">
+    <div className="mt-4 flex items-center justify-center p-8 w-full text-[#462684]">
+      <div className=" md:w-1/2 w-full ">
         <div className="flex space-x-2">
           <button
             className={`${
@@ -48,21 +71,29 @@ export default function Home() {
               : " bg-[#EBFFFC] border-[#59ECD2]"
           }`}
         >
-          <TokenSelector state={state} token={pairToken} onChangeToken={handleChangeToken}/>
+          <TokenSelector
+            state={state}
+            token={pairToken}
+            onChangeToken={handleChangeToken}
+          />
           <div>
             <input
               placeholder="Volum"
               value={volum}
               onChange={onChangeVolum}
               type="text"
-              className={`mt-4 rounded-md form-input w-full border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 ${
-                state === "short" ? "bg-[#FFFAFB]" : "bg-[#F8FFFE]"
+              className={`mt-4 rounded-md form-input w-full border-transparent focus:border-gray-500 border focus:bg-white focus:ring-0 ${
+                state === "short"
+                  ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                  : "bg-[#F8FFFE] border-[#CAFBF2]"
               }`}
             />
           </div>
           <div
-            className={`mt-4 rounded-md p-2.5 flex text-gray-500 w-full ${
-              state === "short" ? "bg-[#FFFAFB]" : "bg-[#F8FFFE]"
+            className={`mt-4 rounded-md p-2.5 flex text-gray-500 w-full border ${
+              state === "short"
+                ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                : "bg-[#F8FFFE] border-[#CAFBF2]"
             }`}
           >
             <span>Price</span>
@@ -70,8 +101,10 @@ export default function Home() {
             <span>$</span>
           </div>
           <div
-            className={`mt-4 rounded-md p-2.5 text-gray-500 w-full ${
-              state === "short" ? "bg-[#FFFAFB]" : "bg-[#F8FFFE]"
+            className={`mt-4 rounded-md p-2.5 text-gray-500 w-full border ${
+              state === "short"
+                ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                : "bg-[#F8FFFE] border-[#CAFBF2]"
             }`}
           >
             <span>
@@ -88,14 +121,63 @@ export default function Home() {
             ></Slider>
           </div>
           <div
-            className={`mt-4 rounded-md p-2.5 text-gray-500 w-full bg-[#E5ECEB] ${
-              state === "short" ? "bg-[#FFFAFB]" : "bg-[#F8FFFE]"
+            className={`mt-4 rounded-md p-2.5 text-gray-500 w-full border bg-[#E5ECEB] ${
+              state === "short"
+                ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                : "bg-[#F8FFFE] border-[#CAFBF2]"
             }`}
           >
             Liquidation Price <br />
             Position Margin <br />
             Fee
           </div>
+          <div className="mt-4">
+            <input
+              className="form-checkbox"
+              id="checkbox"
+              type="checkbox"
+              checked={tlb}
+              onChange={onChangeTlb}
+            />
+            <label
+              className=" ml-2 focus:ring-0 active:ring-0"
+              htmlFor="checkbox"
+            >
+              TP/SL (Entire Position)
+            </label>
+          </div>
+          {tlb && (
+            <div
+              className={`mt-4 rounded-md p-2.5 text-gray-500 w-full border bg-[#E5ECEB] ${
+                state === "short"
+                  ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                  : "bg-[#F8FFFE] border-[#CAFBF2]"
+              }`}
+            >
+              <input
+                placeholder="Take Profit"
+                type="text"
+                value={takeProfit}
+                onChange={onChangeTakeProfit}
+                className={`rounded-md p-2.5 border text-gray-500 w-full bg-[#E5ECEB] ${
+                  state === "short"
+                    ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                    : "bg-[#F8FFFE] border-[#CAFBF2]"
+                }`}
+              />
+              <input
+                placeholder="Stop loss"
+                type="text"
+                value={stopLoss}
+                onChange={onChangeStopLoss}
+                className={`mt-4 rounded-md p-2.5 border text-gray-500 w-full bg-[#E5ECEB] ${
+                  state === "short"
+                    ? "bg-[#FFFAFB] border-[#FFD3DD]"
+                    : "bg-[#F8FFFE] border-[#CAFBF2]"
+                }`}
+              />
+            </div>
+          )}
           <button className="btn w-full mt-4">
             {state === "long" ? "Long" : "Short"}
           </button>
