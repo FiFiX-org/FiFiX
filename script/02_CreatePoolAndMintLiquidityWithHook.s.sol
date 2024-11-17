76ab2bec0dcdf35b6a14b26647c5ce06e4b55cd5
sam <meisamtaher71@gmail.com>
2024-11-17 07:02:25 +0700

    Add HookMiner

--- script/02_CreatePoolAndMintLiquidityWithHook.s.sol
+++ script/02_CreatePoolAndMintLiquidityWithHook.s.sol
@@ -11,7 +11,8 @@ import {TickMath} from "v4-core/src/libraries/TickMath.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-
+import {HookMiner} from "../test/utils/HookMiner.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 
 import {Constants} from "./base/Constants.sol";
 import {Config} from "./base/Config.sol";
@@ -39,13 +40,26 @@ contract CreatePoolAndAddLiquidityWithHookScript is Script, Constants, Config {
     int24 tickLower = -600; // must be a multiple of tickSpacing
     int24 tickUpper = 600;
     /////////////////////////////////////
-    LimitOrder hook;
     function run() external {
         // uint privateKey = vm.envUint("DEV_PRIVATE_KEY");
         // address account = vm.addr(privateKey);
         // vm.startBroadcast(privateKey);
         // // tokens should be sorted
-        hook = new LimitOrder(IPoolManager(address(POOLMANAGER)));
+        // hook contracts must have specific flags encoded in the address
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+        );
+
+        // Mine a salt that will produce a hook address with the correct flags
+        bytes memory constructorArgs = abi.encode(POOLMANAGER);
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(LimitOrder).creationCode, constructorArgs);
+
+        // Deploy the hook using CREATE2
+        vm.broadcast();
+        LimitOrder hook = new LimitOrder{salt: salt}(IPoolManager(address(POOLMANAGER)));
+        require(address(hook) == hookAddress, "CounterScript: hook address mismatch");
         console.log("Hook deployed address", address(hook));
         // PoolKey memory pool = PoolKey({
         //     currency0: currency0,
